7b080409f4751f525ed9cba1c4210af02f3af9ef
Ryan Black <ryan.h.black@gmail.com>
2022-11-02 04:10:24 -0400

    Remove files that should be ignored

--- build/plugin_gui/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marsrover/Plugin-Test/src/plugin_gui
-# Build directory: /home/marsrover/Plugin-Test/build/plugin_gui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rover_tasks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marsrover/Plugin-Test/src/rover_tasks
-# Build directory: /home/marsrover/Plugin-Test/build/rover_tasks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
